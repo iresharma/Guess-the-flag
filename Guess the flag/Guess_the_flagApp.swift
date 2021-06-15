443e2276ad29ca99f0d67ca6e247cbaa151daf3a
Iresharma <iresh.sharma8@gmail.com>
2021-06-16 02:07:16 +0530

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by iresh sharma on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
